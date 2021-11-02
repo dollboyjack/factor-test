732e83f53b21864dc07487f7700fc1ca544c0045
pcxu <54882847+dollboyjack@users.noreply.github.com>
2021-11-02 17:00:06 +0800

    Update factor_test.py

--- factor_test.py
+++ factor_test.py
@@ -8,15 +8,17 @@ import time
 import os
 
 class Context:
-    def __init__(self, f_start_date, start_date, end_date, group_num,Path_factor):
-        self.f_start_date=f_start_date  # 因子开始时间（比如财报1月1日只能用去年的数据，但是因子时间为报告期）
+    def __init__(self, start_date, end_date, group_num, frequency, Path_factor):
         self.start_date=start_date  # 回测区间
         self.end_date=end_date
         self.td=None    # 此时日期：%Y-%m-%d
         self.N=None  # 资产数目
         self.group_num=group_num # 因子组数
+        self.freq=frequency # 调仓频率:日频'd',周频'w',月频'm',财报公布期(5,9,11月)'f'
         self.pos_matrix=None   # 仓位矩阵
         self.net_value=None # 各组+基准净值
+        self.net_value_left=None    # 当前各组合剩余净值
+        self.last_td_mark=None
 
         self.Path_trade_day=None
         self.trade_day=None # 交易日数据
@@ -24,28 +26,40 @@ class Context:
         self.factor=None # 因子数据
         self.Path_price=None
         self.price=None # 股票后复权价格数据
-        self.Path_tradable=None
-        self.tradable=None # 股票是否在市数据
+        self.Path_ST=None
+        self.ST=None # 股票是否ST或退市，是为1，否为0
+        self.Path_suspension=None
+        self.suspension=None # 股票是否停复牌，是为1，否为0
+        self.Path_over1year=None
+        self.over1year=None # 股票是否上市超过1年，是为1，否为0
 
 def initialize(context):
     # 读取信息
     context.Path_trade_day="./data/trade_day.csv"
     context.Path_price="./data/ElementaryFactor-复权收盘价.csv"
-    context.Path_tradable="./data/ElementaryFactor-是否在市.csv"
+    context.Path_ST="./data/ElementaryFactor-ST.csv"
+    context.Path_suspension="./data/ElementaryFactor-停复牌.csv"
+    context.Path_over1year="./data/ElementaryFactor-上市超过一年.csv"
     # 交易日信息
     context.trade_day=pd.read_csv(context.Path_trade_day,parse_dates=['datetime'],index_col=['datetime'])
     context.trade_day=context.trade_day[context.start_date:context.end_date].index
-    # 因子信息，“日期*code”形式
-    context.factor=pd.read_csv(context.Path_factor,parse_dates=['datetime'],index_col=['datetime'])
-    context.factor=context.factor[context.f_start_date:context.end_date]
     # 股价信息
     context.price=pd.read_csv(context.Path_price,parse_dates=['datetime'],index_col=['datetime'])
     context.price=context.price[context.start_date:context.end_date]
-    # 股票是否在市
-    context.tradable=pd.read_csv(context.Path_tradable,parse_dates=['datetime'],index_col=['datetime'])
-    context.tradable=context.tradable[context.start_date:context.end_date]
-    context.tradable=context.tradable.fillna(0)
-    
+    # ST股
+    context.ST=pd.read_csv(context.Path_ST,parse_dates=['datetime'],index_col=['datetime'])
+    context.ST=context.ST[context.start_date:context.end_date]
+    # 是否停牌
+    context.suspension=pd.read_csv(context.Path_suspension,parse_dates=['datetime'],index_col=['datetime'])
+    context.suspension=context.suspension[context.start_date:context.end_date]
+    # 上市满一年
+    context.over1year=pd.read_csv(context.Path_over1year,parse_dates=['datetime'],index_col=['datetime'])
+    context.over1year=context.over1year[context.start_date:context.end_date]
+
+    # 因子信息，“日期*code”形式
+    context.factor=pd.read_csv(context.Path_factor,parse_dates=[0],index_col=[0])
+    context.factor=context.factor[context.start_date:context.end_date]
+        
     context.N=context.factor.shape[1]
     # 净值矩阵初始化
     group_col=['group '+str(i+1) for i in range(context.group_num)]
@@ -58,25 +72,16 @@ def initialize(context):
     context.history_tradable=[]
     context.history_time=[]
 
-def rebalance(context,net_value_left):
-    # 计算使用报告期
-    if context.td.month in [1,2,3,4]:
-        # 使用去年三季报数据
-        financial_time=datetime(context.td.year-1,9,30)
-    elif context.td.month in [5,6,7,8]:
-        # 使用一季报数据
-        financial_time=datetime(context.td.year,3,31)
-    elif context.td.month in [9,10]:
-        # 使用半年报数据
-        financial_time=datetime(context.td.year,6,30)
-    elif context.td.month in [11,12]:
-        # 使用三季报数据
-        financial_time=datetime(context.td.year,9,30)
-    # 得到在市股票的矩阵
-    tradable_matrix=context.tradable.loc[context.td,:].values
+def rebalance(context):
+    # 筛选股票池，去掉ST、上市不满一年、不在市的股票
+    td_ST=context.ST.loc[context.td,:].values
+    td_suspension=context.suspension.loc[context.td,:].values
+    td_over1year=context.over1year.loc[context.td,:].values
+    # 得到可交易股票的矩阵
+    tradable_matrix=(1-td_ST)*(1-td_suspension)*td_over1year
     context.history_tradable.append(tradable_matrix)
     # 用在市公司因子值的均值填充NaN
-    f=context.factor.loc[financial_time,:].values
+    f=context.factor.loc[context.td,:].values
     f_value=f[tradable_matrix==1]
     f[np.isnan(f)]=np.nanmean(f_value)
     context.history_factor.append(f)
@@ -87,10 +92,10 @@ def rebalance(context,net_value_left):
         V_min=np.percentile(f_value,100*g/context.group_num,interpolation='linear')
         V_max=np.percentile(f_value,100*(g+1)/context.group_num,interpolation='linear')
         if g+1 == context.group_num:
-            context.pos_matrix[:,g][(f>=V_min) & (f<=V_max)]=net_value_left[g]
+            context.pos_matrix[:,g][(f>=V_min) & (f<=V_max)]=context.net_value_left[g]
         else:
-            context.pos_matrix[:,g][(f>=V_min) & (f<V_max)]=net_value_left[g]
-    context.pos_matrix[:,context.group_num][tradable_matrix==1]=net_value_left[context.group_num]
+            context.pos_matrix[:,g][(f>=V_min) & (f<V_max)]=context.net_value_left[g]
+    context.pos_matrix[:,context.group_num][tradable_matrix==1]=context.net_value_left[context.group_num]
     # 去掉不在市的权重
     context.pos_matrix=context.pos_matrix*tradable_matrix.reshape([context.N,1])
     # 组内等权
@@ -108,11 +113,8 @@ def calc_IC_ICIR(context):
     for i in range(1,nums_rebalance):
         stock_return=(context.history_price[i]-context.history_price[i-1])/context.history_price[i]
         stock_return=stock_return[context.history_tradable[i-1]==1]
-        # nan值删掉
-        bool_index=(np.isnan(stock_return)) | np.isnan(context.history_factor[i-1][context.history_tradable[i-1]==1])
-        stock_return=stock_return[~bool_index]
+        stock_return[np.isnan(stock_return)]=0  # i或i-1任一期价格不存在设定收益率为0
         factor=context.history_factor[i-1][context.history_tradable[i-1]==1]
-        factor=factor[~bool_index]
         corr=np.corrcoef(stock_return,factor)
         IC.append(corr[0,1])
     ICIR=np.mean(IC)/np.std(IC, ddof=1)
@@ -136,12 +138,20 @@ def summary(context):
     fig = plt.figure(figsize=(12, 9), dpi=100)
     x_label=[t.strftime('%Y-%m-%d') for t in context.history_time[:-1]]
     plt.bar(x_label,IC)
+    if context.freq == 'd':
+        plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(126))
+    elif context.freq == 'w':
+        plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(26))
+    elif context.freq == 'm':
+        plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(6))
+    elif context.freq == 'f':
+        plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(1))
     plt.gca().yaxis.set_major_formatter(ticker.PercentFormatter(xmax=1, decimals=2))
     plt.grid(axis="y")
     plt.xticks(rotation=-45)
     plt.title(title)
     #plt.show()
-    plt.savefig('./result/IC_'+factor_name+'.png')
+    plt.savefig('./result/IC_'+factor_name+'_'+context.freq+'.png')
     # 多空图
     fig = plt.figure(figsize=(12, 9), dpi=100)    
     ax = fig.add_subplot(2, 1, 1)
@@ -156,32 +166,45 @@ def summary(context):
     plt.xticks(rotation=-45)
     ax.legend()
     #plt.show()
-    plt.savefig('./result/L-S_'+factor_name+'.png')
+    plt.savefig('./result/L-S_'+factor_name+'_'+context.freq+'.png')
 
-def handle_data(context, td_num, last_td_month):
-    rebalance_month=[5,9,11]
-    if td_num == 0:
+def handle_data(context):
+    if not context.last_td_mark:
         # 最初建仓
-        net_value_left=context.net_value.iloc[td_num,:].values
-        rebalance(context,net_value_left)
+        context.net_value_left=context.net_value.loc[context.td,:].values
+        rebalance(context)
     else:
-        if context.td.month in rebalance_month and context.td.month != last_td_month:
-            # 5,9,11的第一个交易日换仓,更新仓位矩阵
-            net_value_left=context.net_value.iloc[td_num-1,:].values
-            rebalance(context,net_value_left)
         # 利用仓位矩阵计算净值
         td_price=context.price.loc[context.td,:].fillna(value=0)
         context.net_value.loc[context.td,:]=td_price.dot(context.pos_matrix)
+        # 更新剩余净值
+        context.net_value_left=context.net_value.loc[context.td,:].values
+
+        rebalance_month=[5,9,11]
+        # 调仓
+        if context.freq == 'd':
+            # 每日换仓
+            rebalance(context)
+        elif context.freq == 'w' and (context.td.strftime('%W') != context.last_td_mark):
+            # 每周换仓
+            rebalance(context)
+        elif context.freq == 'm' and (context.td.month != context.last_td_mark):
+            # 每月换仓
+            rebalance(context)
+        elif context.freq == 'f' and (context.td.month in rebalance_month and context.td.month != context.last_td_mark):
+            # 5,9,11的第一个交易日换仓,更新仓位矩阵
+            rebalance(context)
 
 def run(context):
     initialize(context)
-    last_td_month=12 if int(context.start_date[4:6]) == 1 else int(context.start_date[4:6])-1
-    td_num=-1
     for td in context.trade_day:
-        td_num+=1
         context.td=td
-        handle_data(context, td_num, last_td_month)
-        last_td_month=td.month
+        handle_data(context)
+        # 周/月更改标记，用于判断是否换仓
+        if context.freq == 'w':
+            context.last_td_mark=td.strftime('%W')
+        else:
+            context.last_td_mark=td.month
     summary(context)
 
 
@@ -190,11 +213,5 @@ file_path="D:/study/compile/research/factor_database/corporation_factor/profit_f
 file_list=os.listdir(file_path)
 #file_list=['roe.csv',]
 for f in file_list:
-    context=Context('20160630','20170101','20210630',10,file_path+f)
+    context=Context('20170101', '20210630', 10, 'f', file_path+f)
     run(context)
-
-#################################################################
-'''
-遗留问题：
-1.日频、周频、月频的调仓补充
-'''
